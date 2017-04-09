dc08dd182c917b5f142836721e98b26f554ea572
Blaise Wang <blaisewang@icloud.com>
2017-04-09 23:04:19 +0800

    minor improve

--- src/lab4/LongestCommonSubsequence.java
+++ src/lab4/LongestCommonSubsequence.java
@@ -10,39 +10,38 @@ public class LongestCommonSubsequence {
 
     private static String first;
     private static String second;
-    private static int[][] commonArray;
+    private static int firstLength;
+    private static int secondLength;
+    private static int[][] table;
     private static StringBuilder stringBuilder = new StringBuilder();
 
-    private static void longestCommonSubsequence(String first, String second) {
-        int firstLength = first.length();
-        int secondLength = second.length();
-        commonArray = new int[firstLength + 1][secondLength + 1];
+    private static void longestCommonSubsequence() {
         for (int row = 0; row <= firstLength; row++) {
             for (int column = 0; column <= secondLength; column++) {
-                if (column == 0 || row == 0) {
-                    commonArray[row][column] = 0;
-                } else if (first.charAt(row - 1) == second.charAt(column - 1)) {
-                    commonArray[row][column] = commonArray[row - 1][column - 1] + 1;
-                } else {
-                    commonArray[row][column] = commonArray[row - 1][column] >= commonArray[row][column - 1] ? commonArray[row - 1][column] : commonArray[row][column - 1];
+                if (row != 0 && column != 0) {
+                    if (first.charAt(row - 1) != second.charAt(column - 1)) {
+                        table[row][column] = table[row - 1][column] >= table[row][column - 1] ? table[row - 1][column] : table[row][column - 1];
+                    } else {
+                        table[row][column] = table[row - 1][column - 1] + 1;
+                    }
                 }
             }
         }
     }
 
-    private static void getLCS(int row, int cloumn) {
-        if (row == 0 || cloumn == 0) {
+    private static void getLCS(int row, int column) {
+        if (row == 0 || column == 0) {
             return;
         }
 
-        if (first.charAt(row - 1) != second.charAt(cloumn - 1) || commonArray[row][cloumn] != commonArray[row - 1][cloumn - 1] + 1) {
-            if (first.charAt(row - 1) != second.charAt(cloumn - 1) && commonArray[row - 1][cloumn] >= commonArray[row][cloumn - 1]) {
-                getLCS(row - 1, cloumn);
+        if (first.charAt(row - 1) != second.charAt(column - 1) || table[row][column] != table[row - 1][column - 1] + 1) {
+            if (first.charAt(row - 1) != second.charAt(column - 1) && table[row - 1][column] >= table[row][column - 1]) {
+                getLCS(row - 1, column);
             } else {
-                getLCS(row, cloumn - 1);
+                getLCS(row, column - 1);
             }
         } else {
-            getLCS(row - 1, cloumn - 1);
+            getLCS(row - 1, column - 1);
             stringBuilder.append(first.charAt(row - 1));
         }
     }
@@ -54,9 +53,12 @@ public class LongestCommonSubsequence {
         System.out.println("Second string:");
         second = scanner.nextLine();
         scanner.close();
-        longestCommonSubsequence(first, second);
-        getLCS(first.length(), second.length());
-        System.out.println("Length: " + commonArray[first.length()][second.length()]);
+        firstLength = first.length();
+        secondLength = second.length();
+        table = new int[firstLength + 1][secondLength + 1];
+        longestCommonSubsequence();
+        getLCS(firstLength, secondLength);
+        System.out.println("Length: " + table[firstLength][secondLength]);
         System.out.println("LCS: " + stringBuilder.toString());
     }
 }
