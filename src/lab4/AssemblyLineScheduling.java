0c9418777311ba7f734edb09e8d27a2a8ab0ce95
Blaise Wang <blaisewang@icloud.com>
2017-04-10 10:37:51 +0800

    minor improve

---
+++ src/lab4/AssemblyLineScheduling.java
@@ -0,0 +1,11 @@
+package lab4;
+
+/**
+ * AlgorithmAnalysis
+ * Created by blaisewang on 2017/4/10.
+ */
+public class AssemblyLineScheduling {
+    public static void main(String[] args) {
+
+    }
+}
